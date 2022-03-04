3821e35f3d60ffbd693af6a8b9e806b2c6c455bb
Rishabh Gupta <rishabhg.1996@gmail.com>
2022-03-04 08:14:09 +0530

    yes

---
+++ sprint-1/day-2/assignments/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() { 
+  return(
+    <div className="App">
+    <h1>Mobile Operating System</h1>
+    <div>
+      <ul>
+        <li>Android</li>
+        <li>BlackBerry</li>
+        <li>iPhone</li>
+        <li>Windows Phone</li>
+      </ul>
+    </div>
+    <h1>Mobile Manufacturers</h1>
+    <div>
+      <ul>
+        <li>Samsung</li>
+        <li>HTC</li>
+        <li>Micromax</li>
+        <li>Apple</li>
+      </ul>
+    </div>
+    </div>
+  );
+}
+
+export default App;
