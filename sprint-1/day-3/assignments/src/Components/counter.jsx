70f96a8df4fa5b21ee8503b1b111fdfd8fda4277
Rishabh Gupta <rishabhg.1996@gmail.com>
2022-03-04 21:48:48 +0530

    assignemnt_3

---
+++ sprint-1/day-3/assignments/src/Components/counter.jsx
@@ -0,0 +1,53 @@
+import {useState} from "react"
+import '../App.css';
+
+
+
+function Counter(){
+
+    const [counter, setCounter] = useState(0)
+
+    const multiply = () => {
+        setCounter(counter * 2)
+    }
+ 
+   const add = (value) => {
+        setCounter(counter + value)
+    }
+
+
+    return (
+
+        <div>
+            <h1>Counter : {counter}</h1>
+            <button onClick={
+                () => {
+                    add(1)
+                }
+            }>      
+                Add-1
+            </button>
+            <br />
+            <button onClick={
+                () => {
+                    if(counter > 0){
+                        add(-1)
+                    }
+                }
+            }>
+                Sub-1
+            </button>
+            <br />
+            <button onClick={
+                () => {
+                   multiply()
+                }
+            }>
+                Multiply-2
+            </button>
+        </div>        
+
+    )
+}
+
+export default Counter
--- sprint-1/day-3/assignments/src/index.js
+++ sprint-1/day-3/assignments/src/index.js
@@ -1,60 +1,17 @@
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-
-const logoimg = document.createElement("img");
-logoimg.src="https://media.glassdoor.com/sqll/3235648/masai-school-squarelogo-1645792545584.png";
-logoimg.classList.add("logo");
-
-document.querySelector("#logo").append(logoimg);
-
-let inputdiv=document.querySelector("#hey");
-let count=0;
-
-let arr=JSON.parse(localStorage.getItem("webpack"))||[];
-showData(arr);
-document.querySelector("#submitpart").addEventListener("click",function(){
-    count++;
-
-    let note = document.querySelector("#inputvalue").value;
-    let obj={
-        message : note
-    };
-    arr.push(obj);
-    localStorage.setItem("webpack",JSON.stringify(arr));
-
-    let data = JSON.parse(localStorage.getItem("webpack"))
-    showData(data);
-})
-
-
-function showData(arr){
-    inputdiv.innerHTML="";
-    console.log(arr);
-    arr.forEach((elem,index)=>{
-        let number = document.createElement("p");
-        number.textContent= index+1;
-        number.classList.add("number");
-        let note = document.createElement("p");
-        note.textContent=elem.message;
-        note.classList.add("note");
-        
-        let del = document.createElement("button");
-        del.textContent="delete";
-        del.addEventListener("click",function(){
-            deleteData(elem,index)
-        })
-
-        let eachrow= document.createElement("div");
-        eachrow.append(number,note,del);
-        eachrow.classList.add("eachrow");
-        document.querySelector("#hey").append(eachrow);    
-    })
-}
-
-
-function deleteData(elem,index){
-    arr.splice(index,1);
-    localStorage.setItem("webpack",JSON.stringify(arr));
-    showData(arr);
-}
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
